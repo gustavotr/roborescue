c552f2e7b1a7fa329f009f19e9e72df2c4614ac1
Gustavo Teixeira Rüdiger <gustavotrudiger@gmail.com>
2013-08-30 14:19:11 -0300

    Alterações na movimentação dos robos

--- Roborescue/src/roborescue/TimeATeamEnv.java
+++ Roborescue/src/roborescue/TimeATeamEnv.java
@@ -4,6 +4,7 @@ package roborescue;
 import jason.RoborescueEnv;
 import jason.asSyntax.Structure;
 
+import java.awt.geom.Point2D;
 import java.rmi.RemoteException;
 
 import robocode.rescue.RobotInfo;
@@ -45,8 +46,62 @@ public class TimeATeamEnv extends RoborescueEnv {
         double yRefem = refem.getY();
         RMIRobotInterface[] teamRef = getTeamRef();
 
-        for (int robo = 1; robo < numRobos; robo++) {            
+        for (int robo = 1; robo < numRobos; robo++) { 
+        	
+        	Point2D from = new Point2D.Double(robos[robo].getX(), robos[robo].getY());
+			double toX = 0;
+			double toY = 0;	
             
+			
+			String str = robos[robo].getName();
+			str = str.substring(15,16);
+			int num = Integer.parseInt(str);					
+			switch(num){
+				case 1:
+					if(teamRef[0].isFollowing() == 1){
+						toX = xRefem;
+						toY = yRefem;
+					}
+					else{						
+						toX = 2500;
+						toY = 1400;
+					}												
+					break;
+				/*case 2:
+					toX = 1700;
+					toY = enemys[2].getY();
+					break;
+				case 3:
+					toX = 1700;
+					toY = enemys[3].getY();
+					break;
+				case 4:
+					if(id == 1.0){ //Robo4 ao ataque	
+						if(getReferenciaServidor().isRefemFollowing(myTeam)){
+							refemStatus = 1;
+							perc = Literal.parseLiteral("status(jogo, "+ataque()+", "+defesa()+", 1)"); 
+							addPercept(perc);
+						}
+						toX = xRefem;
+						toY = yRefem;						
+					}
+					if(id == 1.1){ //Robo4 bloqueado						
+						toX = xRefem;
+						toY = yRefem;
+					}
+					if(id == 2){						
+						toX = 2500;
+						toY = 100;
+					}			
+					break;*/
+			}		
+					
+			Point2D to = new Point2D.Double(toX,toY);						
+			goTo(from, to, robos[robo], teamRef);	 
+			
+			
+			
+			/*
             if (xRefem > robos[robo].getX() && yRefem > robos[robo].getY()) {
                 teamRef[robo].setTurnRight(0);
             } else if (xRefem > robos[robo].getX() && yRefem < robos[robo].getY()) {
@@ -65,7 +120,7 @@ public class TimeATeamEnv extends RoborescueEnv {
                 } else {
                     teamRef[robo].setAhead(100.0);
                 }
-            }
+            }*/
             
             teamRef[robo].execute();
 
@@ -97,6 +152,38 @@ public class TimeATeamEnv extends RoborescueEnv {
             ex.printStackTrace();
           }
         }
-    }    
+    }  
+    
+    
+    //funçoes para locomoção
+    private double absoluteBearing(Point2D source, Point2D target) {
+        return Math.toDegrees(Math.atan2(target.getX() - source.getX(), target.getY() - source.getY()));
+   }
+
+   private double normalRelativeAngle(double angle) {
+       double relativeAngle = angle % 360;
+       if (relativeAngle <= -180)
+           return 180 + (relativeAngle % 180);
+       else if (relativeAngle > 180)
+           return -180 + (relativeAngle % 180);
+       else
+           return relativeAngle;
+   }
+	
+   //retorna um ponto onde a coordenada x é o angulo e o y é a distancia a ser percorrida
+	private Point2D goTo(Point2D from, Point2D to, RobotInfo robo, RMIRobotInterface[] teamRef) {
+       double distance = from.distance(to);
+       double angle = normalRelativeAngle(absoluteBearing(from, to) - robo.getHeading());
+       if (Math.abs(angle) > 90.0) {
+           distance *= -1.0;
+           if (angle > 0.0) {
+               angle -= 180.0;
+           }
+           else {
+               angle += 180.0;
+           }
+       }	
+       return new Point2D.Double(angle,distance);       
+   }
     
 }
